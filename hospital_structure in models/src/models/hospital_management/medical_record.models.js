45c4a1966f2e736b0c707cdc17de0e25631f6bd1
Suraj Singh <139257270+SurajTrainer@users.noreply.github.com>
2024-05-06 13:09:40 +0530

    medical_record.models

--- hospital_structure in models/src/models/hospital_management/medical_record.models.js
+++ hospital_structure in models/src/models/hospital_management/medical_record.models.js
@@ -1,6 +1,23 @@
 
 import mongoose from 'mongoose'
 
-const doctorSchema = new Schema({},{timestamps : true})
+const medical_recordSchema = new Schema({
+  name : {
+    type : String,
+    required : true,
+  },
+  address : {
+    type : String,
+    required : true,
+  },
+  problems : {
+    type : String,
+    required : true,
+  },
+  injury : {
+    type : String,
+    required : true,
+  }
+},{timestamps : true})
 
-export const Doctor = mongoose.model("Doctor", doctorSchema)
+export const MedicalRecord = mongoose.model("MedicalRecord", medical_recordSchema)
