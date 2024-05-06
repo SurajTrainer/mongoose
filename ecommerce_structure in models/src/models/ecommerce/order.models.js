bb9b0f40ada9e12b9ed7ac40537803f025cadee2
Suraj Singh <139257270+SurajTrainer@users.noreply.github.com>
2024-05-06 11:41:30 +0530

    Update order.models.js

---
+++ ecommerce_structure in models/src/models/ecommerce/order.models.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const orderItemSchema = new mongoose.Schema({
+  productId : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'Product'
+  },
+  quantity : {
+    type : Number,
+    required : true
+  }
+})
+
+const orderSchema = new Schema({
+  orderPrice : {
+    type : Number,
+    required : true
+  },
+  customer : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : "User"
+  },
+  orderItem : [orderItemSchema],
+  address : {
+    type : String,
+    required : true,
+  },
+  status : {
+    type : String,
+    enum : ["PENDING", "CANCELED","DELIVERED"],
+    default : "PENDING"
+  }
+} , {timestamps : true})
+
+export const Order = mongoose.model("Order", orderSchema)
